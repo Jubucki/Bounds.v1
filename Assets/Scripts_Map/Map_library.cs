26bc6e8aeb51aa253db92febc6b144d2fa736fc4
Julien Robert Beranek <beranek0julian@gmail.com>
2026-08-07 22:51:28 +0200

    Update.v2

--- Assets/Scripts_Map/Map_library.cs
+++ Assets/Scripts_Map/Map_library.cs
@@ -32,7 +32,7 @@ public class Map_library : MonoBehaviour
         return mirrored;
     }
 
-    public GeneratedMap BuildSymmetricMap(List<PlatformData> halfMap, Vector 2 center)
+    public GeneratedMap BuildSymmetricMap(List<PlatformData> halfMap, Vector2 center)
     {
         GeneratedMap fullMap = new GeneratedMap();
 
@@ -45,7 +45,56 @@ public class Map_library : MonoBehaviour
 
         return fullMap;
     }
-    
+
+    public List<PlatformData> GenerateHalfMap (int platformCount, Rect bounds, float minDistance, PlatformType[] possibleTypes )
+    {
+        List<PlatformData> result = new List<PlatformData>();
+        int maxAttempts = 50;
+
+        for (int i = 0; i < platformCount; i++)
+        {
+            bool placed =false;
+            int attempts = 0;
+
+            while (!placed && attempts < maxAttempts)
+            {
+                attempts++;
+
+                Vector2 candidatePos = new Vector2(
+                    UnityEngine.Random.Range(bounds.xMin, bounds.xMax),
+                    UnityEngine.Random.Range(bounds.yMin, bounds.yMax)
+                );
+
+                if (IsFarEnough(candidatePos, result, minDistance))
+                {
+                    PlatformData newPlatform = new PlatformData
+                    {
+                        position = candidatePos,
+                        size = new Vector2(2f, 0.5f),
+                        type = possibleTypes[UnityEngine.Random.Range(0, possibleTypes.Length)]
+                    };
+
+                    result.Add(newPlatform);
+                    placed = true;
+                }
+            }
+        }
+
+        return result;
+
+    }
+
+    private bool IsFarEnough(Vector2 candidate, List<PlatformData> existing, float minDistance)
+    {
+        foreach (PlatformData platform in existing)
+        {
+            if(Vector2.Distance(candidate, platform.position) < minDistance)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
